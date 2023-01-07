19e8f546a5031c638fb7b8857c81bd0167d8f536
Cristian Perdomo <cristian2020til@gmail.com>
2023-01-07 12:24:29 -0500

    migraciones y modelos avance de proyecto rest

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /* realacion uno a muchos */
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
 }
